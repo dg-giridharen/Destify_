c720bdede82f2e4a694d6fa79edc19a42e37b70b
Giridharen G <160459353+dg-giridharen@users.noreply.github.com>
2025-07-01 22:13:25 +0530

    Update server.js

--- destifybackend/server.js
+++ destifybackend/server.js
@@ -1,4 +1,3 @@
- 
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
@@ -11,19 +10,25 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: [
+        "https://destify-admin.onrender.com",
+        "https://destify-r6l0.onrender.com"  
+    ]
+})); 
 
 connectDB();
 
-app.use("/api/destination", destinationRouter);
-app.use("/api/user", userRouter);
-app.use("/api/order", orderRouter);
-app.use("/images", express.static('uploads')); // Serve uploaded images
+app.use("/api/destination", destinationRouter); //
+app.use("/api/user", userRouter); //
+app.use("/api/order", orderRouter); //
+app.use("/images", express.static('uploads'));
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running..."); 
 });
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`); 
 });
